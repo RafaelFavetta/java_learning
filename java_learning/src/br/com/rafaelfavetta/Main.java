14d348f55fc18571188de35c9a4e90f30ce7d28e
Rafael Favetta <rafaelfavetta@gmail.com>
2025-12-15 15:40:48 -0300

    estudando laços de repetição (while, do-while, for)

--- java_learning/src/br/com/rafaelfavetta/Main.java
+++ java_learning/src/br/com/rafaelfavetta/Main.java
@@ -3,10 +3,22 @@ public class Main {
         
         byte diaSemana = 0;
 
-        switch (diaSemana) {
-            case 1, 7 -> System.out.println("Fim de semana");
-            case 2, 3, 4, 5, 6 -> System.out.println("Dia útil");
-            default -> throw new IllegalArgumentException("Dia inválido: " + diaSemana);
+        // switch (diaSemana) {
+        //     case 1, 7 -> System.out.println("Fim de semana");
+        //     case 2, 3, 4, 5, 6 -> System.out.println("Dia útil");
+        //     default -> throw new IllegalArgumentException("Dia inválido: " + diaSemana);
+        // }
+
+        // while (diaSemana<15) {
+        //     System.out.println("Dia da semana: " + ++diaSemana);
+        // }
+
+        // do {
+        //     System.out.println("Dia da semana: " + ++diaSemana);
+        // } while (diaSemana < 20);
+
+        for(diaSemana = 7; diaSemana >= 1; diaSemana--) {
+            System.out.println("Dia da semana: " + diaSemana);
         }
     }
 }
