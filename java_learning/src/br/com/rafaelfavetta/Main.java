f04fb66ac28ad9028d0d048793a15cd4e57751d2
Rafael Favetta <rafaelfavetta@gmail.com>
2025-12-15 15:32:37 -0300

    outra maneira possível de se resolver switch case a partir do Java 14

--- java_learning/src/br/com/rafaelfavetta/Main.java
+++ java_learning/src/br/com/rafaelfavetta/Main.java
@@ -1,23 +1,12 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         
-        byte diaSemana = 1;
+        byte diaSemana = 0;
 
         switch (diaSemana) {
-            case 1:
-            case 7:
-                System.out.println("Fim de semana!");
-                break;
-            case 2:
-            case 3:
-            case 4:
-            case 5:
-            case 6:
-                System.out.println("Dia útil!");
-                break;       
-            default:
-                System.out.println("Dia da semana inválido!");
-                break;
+            case 1, 7 -> System.out.println("Fim de semana");
+            case 2, 3, 4, 5, 6 -> System.out.println("Dia útil");
+            default -> throw new IllegalArgumentException("Dia inválido: " + diaSemana);
         }
     }
 }
