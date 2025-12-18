4754010f7daa0a0a63f4f53ba93d4edb3d7f3244
Rafael Favetta <rafaelfavetta@gmail.com>
2025-12-18 15:17:02 -0300

    revisão do operador ternário

--- java_learning/src/main/java/br/com/rafaelfavetta/app/IfElse.java
+++ java_learning/src/main/java/br/com/rafaelfavetta/app/IfElse.java
@@ -3,14 +3,17 @@ package br.com.rafaelfavetta.app;
 public class IfElse {
     public static void main(String[] args) {
 
-        double notaAluno = 4.99;
+        double notaAluno = 8;
 
-        if (notaAluno >= 7){
-            System.out.println("Aprovado.");
-        } else if (notaAluno >= 5) {
-            System.out.println("Recuperação.");
-        } else {
-            System.out.println("Reprovado.");
-        }
+//        if (notaAluno >= 7){
+//            System.out.println("Aprovado.");
+//        } else if (notaAluno >= 5) {
+//            System.out.println("Recuperação.");
+//        } else {
+//            System.out.println("Reprovado.");
+//        }
+
+        String resultadoFinal = notaAluno >= 7 ? "Aprovado" : notaAluno >=5 ? "Recuperação" : "Reprovado";
+        System.out.println(resultadoFinal);
     }
 }
