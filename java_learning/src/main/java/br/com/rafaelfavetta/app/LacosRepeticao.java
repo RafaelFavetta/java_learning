ed66ce25107c453cc8f56045eb6fcfd4900ddd9d
Rafael Favetta <rafaelfavetta@gmail.com>
2025-12-18 15:30:40 -0300

    revisão de WHILE, DO-WHILE e FOR

---
+++ java_learning/src/main/java/br/com/rafaelfavetta/app/LacosRepeticao.java
@@ -0,0 +1,22 @@
+package br.com.rafaelfavetta.app;
+
+public class LacosRepeticao {
+    public static void main(String[] args) {
+
+        int i = 1;
+
+//        while (i < 101){
+//            System.out.println(i);
+//            i++;
+//        }
+
+//        do {
+//            System.out.println(i);
+//            i+=2;
+//        } while (i <= 101);
+
+        for (i = 0; i <= 500 ; i+=55){
+            System.out.println(i);
+        }
+    }
+}
