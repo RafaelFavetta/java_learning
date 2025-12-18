2def951a889739c28804d2f95405135a65cf3779
Rafael Favetta <rafaelfavetta@gmail.com>
2025-12-18 14:37:10 -0300

    começo da revisão antes de começar Orientação a Objetos

---
+++ java_learning/src/main/java/br/com/rafaelfavetta/app/TiposPrimitivos.java
@@ -0,0 +1,15 @@
+package br.com.rafaelfavetta.app;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+
+        int idade = 44;
+        double altura = 1.80;
+        char inicial = 'R';
+        boolean isEstudante = true;
+        double media = 7.8;
+
+        System.out.println("Idade: " +idade + "\nAltura: " +altura + "\nInicial: " +inicial + "\nÉ estudante?: " +isEstudante);
+        System.out.println("Média: " +(int)media);
+    }
+}
