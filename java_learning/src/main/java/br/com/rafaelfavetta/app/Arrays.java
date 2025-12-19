0e2962facab243ffdf228963b64c9e003d30f0aa
Rafael Favetta <rafaelfavetta@gmail.com>
2025-12-19 14:37:08 -0300

    revisão de arrays com FOR

---
+++ java_learning/src/main/java/br/com/rafaelfavetta/app/Arrays.java
@@ -0,0 +1,12 @@
+package br.com.rafaelfavetta.app;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        int[] arrBasico = {1,2,3,4,5,6,7,8,9,10,11,12};
+
+        for (int i = 0; i < arrBasico.length; i++){
+            System.out.println(arrBasico[i]);
+        }
+    }
+}
