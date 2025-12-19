23a29913f37fad695c57e7031be317f9fa288ff1
Rafael Favetta <rafaelfavetta@gmail.com>
2025-12-19 14:41:40 -0300

    revisão de arrays com FOR pt2

--- java_learning/src/main/java/br/com/rafaelfavetta/app/Arrays.java
+++ java_learning/src/main/java/br/com/rafaelfavetta/app/Arrays.java
@@ -3,10 +3,14 @@ package br.com.rafaelfavetta.app;
 public class Arrays {
     public static void main(String[] args) {
 
-        int[] arrBasico = {1,2,3,4,5,6,7,8,9,10,11,12};
+        int[] arrBasico = {1,2,3,4,5,6,7,8,9,10,14,16};
 
         for (int i = 0; i < arrBasico.length; i++){
             System.out.println(arrBasico[i]);
         }
+
+        System.out.println("Esse é o tamanho do array: " + arrBasico.length);
+        System.out.println("Esse é o primeiro elemento do array: " + arrBasico[0]);
+        System.out.println("Esse é o último elemento do array: " + arrBasico[arrBasico.length - 1]);
     }
 }
