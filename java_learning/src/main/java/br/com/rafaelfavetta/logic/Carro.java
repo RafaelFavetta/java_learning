96b848f8e3f45421a3ece228def9ab3923bb8413
Rafael Favetta <rafaelfavetta@gmail.com>
2025-12-23 14:06:33 -0300

    início da POO

---
+++ java_learning/src/main/java/br/com/rafaelfavetta/logic/Carro.java
@@ -0,0 +1,8 @@
+package br.com.rafaelfavetta.logic;
+
+public class Carro {
+
+    public String nome;
+    public String modelo;
+    public int ano;
+}
---
+++ java_learning/src/main/java/br/com/rafaelfavetta/model/CarroTest.java
@@ -0,0 +1,24 @@
+package br.com.rafaelfavetta.model;
+
+import br.com.rafaelfavetta.logic.Carro;
+
+public class CarroTest {
+    public static void main(String[] args) {
+
+        Carro mustang = new Carro();
+        mustang.ano = 2011;
+        mustang.modelo = "V8";
+        mustang.nome = "Mustang Prolific";
+
+        System.out.println(mustang.nome + " " + mustang.modelo + " " + mustang.ano);
+
+        Carro bugatti = new Carro();
+        bugatti.ano = 2015;
+        bugatti.modelo = "V8";
+        bugatti.nome = "Bugatti Veyron";
+
+        System.out.println(bugatti.nome + " " + bugatti.modelo + " " + bugatti.ano);
+    }
+
+
+}
