fb90fc5c7e1464e4207f50a89c7167f9675e2fdc
Rafael Favetta <rafaelfavetta@gmail.com>
2026-01-05 15:00:25 -0300

    exercício de métodos de referência

---
+++ java_learning/src/main/java/br/com/rafaelfavetta/logic/Funcionario.java
@@ -0,0 +1,29 @@
+package br.com.rafaelfavetta.logic;
+
+public class Funcionario {
+
+    public String nome;
+    public int idade;
+    public double[] salarios;
+
+    public void imprimeDados() {
+        System.out.println(this.nome);
+        System.out.println(this.idade);
+
+        for (double salario : salarios){
+            System.out.println(salario);
+        }
+
+        imprimeMediaSalarial();
+    }
+
+    public void imprimeMediaSalarial(){
+        double media = 0;
+
+        for (double salario : salarios){
+            media += salario;
+        }
+        media /= salarios.length;
+        System.out.println("Média: " + media);
+    }
+}
---
+++ java_learning/src/main/java/br/com/rafaelfavetta/model/FuncionarioTest.java
@@ -0,0 +1,15 @@
+package br.com.rafaelfavetta.model;
+
+import br.com.rafaelfavetta.logic.Funcionario;
+
+public class FuncionarioTest {
+    public static void main(String[] args) {
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.nome = "Jair";
+        funcionario.idade = 19;
+        funcionario.salarios = new double[]{1200, 999.9, 200.44};
+
+        funcionario.imprimeDados();
+    }
+}
