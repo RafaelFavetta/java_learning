6dd5475f5d9ee8e758895117209c6fedde4de688
Rafael Favetta <rafaelfavetta@gmail.com>
2026-01-14 15:16:41 -0300

    refazendo o exercicio com List

--- java_learning/src/main/java/br/com/rafaelfavetta/exercicios/polymorphism/ex1/test/StaffTest01.java
+++ java_learning/src/main/java/br/com/rafaelfavetta/exercicios/polymorphism/ex1/test/StaffTest01.java
@@ -4,24 +4,25 @@ import br.com.rafaelfavetta.exercicios.polymorphism.ex1.domain.Developer;
 import br.com.rafaelfavetta.exercicios.polymorphism.ex1.domain.Manager;
 import br.com.rafaelfavetta.exercicios.polymorphism.ex1.domain.Staff;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StaffTest01 {
     public static void main(String[] args) {
 
-        Staff stf1 = new Manager("JoJo", 6000);
-        Staff stf2 = new Manager("Quark", 4000);
-        Staff stf3 = new Developer("Earl", 499.90);
-        Staff stf4 = new Developer("Ragnar", 899);
-        Staff stf5 = new Developer("Lagaertha", 1899.90);
+        List<Staff> staffs = new ArrayList<>();
 
-        System.out.println("Bônus de " + stf1.getName() + ": " + stf1.calculateBonus());
-        System.out.println("Salário total: " + stf1.calculateTotalSalary());
-        System.out.println("\nBônus de " + stf2.getName() + ": " + stf2.calculateBonus());
-        System.out.println("Salário total: " + stf2.calculateTotalSalary());
-        System.out.println("\nBônus de " + stf3.getName() + ": " + stf3.calculateBonus());
-        System.out.println("Salário total: " + stf3.calculateTotalSalary());
-        System.out.println("\nBônus de " + stf4.getName() + ": " + stf4.calculateBonus());
-        System.out.println("Salário total: " + stf4.calculateTotalSalary());
-        System.out.println("\nBônus de " + stf5.getName() + ": " + stf5.calculateBonus());
-        System.out.println("Salário total: " + stf5.calculateTotalSalary());
+        staffs.add(new Manager("JoJo", 6000));
+        staffs.add(new Manager("Quark", 4000));
+        staffs.add(new Developer("Earl", 499.90));
+        staffs.add(new Developer("Ragnar", 899));
+        staffs.add(new Developer("Lagaertha", 1899.90));
+
+        for (Staff s : staffs) {
+            System.out.println(s.getName());
+            System.out.println(s.calculateBonus());
+            System.out.println(s.calculateTotalSalary());
+            System.out.println();
+        }
     }
 }
